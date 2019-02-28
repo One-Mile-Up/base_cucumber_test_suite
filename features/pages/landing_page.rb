c7c773f3bf7d9aebb2cefe8c4818c8b9667772c1
Darryl Pequeen II <pequeeniid@aetna.com>
2019-02-28 12:02:04 -0700

    - Add .gitignore

--- features/pages/landing_page.rb
+++ features/pages/landing_page.rb
@@ -2,7 +2,7 @@ class LandingPage
   class << self 
 
     # This is a WIP --  more to come!
-    
+
     # element   :article_title,               '[data-test="article-title"]'
     # element   :related_articles_container,  '[data-test="article-preview-container"]'
     # elements  :related_article_buttons,     '[data-test="article-preview-button"]'
