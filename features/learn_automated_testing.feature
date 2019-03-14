0a9450f92a8c0ace3d3e9ebe26881002b441a73b
Alexander Rau <alexanderrau86@yahoo.com>
2019-03-14 17:00:42 -0600

    Add some tests

--- features/learn_automated_testing.feature
+++ features/learn_automated_testing.feature
@@ -5,3 +5,32 @@ Feature: Learn Automated Testing
      When   I log into the application with user "standard_user"
      Then   I see the homepage 
     
+  Scenario: I add an item to the shopping cart
+    Given   I am logged in with user "standard_user" and the cart is empty
+     When   I click to add the item I want
+     Then   I see number of items in the shopping cart increment up by 1
+
+  Scenario: I remove an item from the shopping cart
+    Given   I have an item in my shopping cart
+     When   I click on the remove button
+     Then   I see the number of items in the cart decrease by 1
+
+  Scenario: I visit the shopping cart
+    Given   I log into the products page with user "standard_user"
+     When   I click on the shopping cart
+     Then   I can see the items I have selected
+
+  Scenario: I resume shopping
+    Given   I login with user "standard_user" and enter the shopping cart
+     When   I click the Continue Shopping link
+     Then   I am returned to the products page
+
+  Scenario: I navigate to the About page
+    Given   I log into the products page with user "standard_user"
+     When   I click on the hamburger menu and then on the About prompt
+     Then   I see the Sauce Labs home page
+
+  Scenario: I log out of the Swag Labs practice website
+    Given   I am logged in
+     When   I click on the hamburger menu and click to log out
+     Then   I see the login page
--- features/step_definitions/learn_automated_testing_steps.rb
+++ features/step_definitions/learn_automated_testing_steps.rb
@@ -9,10 +9,83 @@ When(/^I log into the application with user "(.*?)"$/) do |user|
   # Fill in the password
   find('[data-test="password"]').send_keys('secret_sauce')
   # Hit the login button
-  find('.login-button').click
+  find('.btn_action').click
 end
 
 Then("I see the homepage") do
-  binding.pry
-  pending # Write code here that turns the phrase above into concrete actions
+  have_selector('div.inventory_list')
+end
+
+# Given(/^I am logged in with user "(.*?)" and the cart is empty$/) do |user|
+#   find('[data-test="username"]').send_keys(user)
+#   find('[data-test="password"]').send_keys('secret_sauce')
+#   find('.btn_action').click
+#   have_selector('div.inventory_list')
+# end
+
+# When(/^I click to add the item I want$/) do
+#   click_on(class: 'btn_primary')
+# end
+
+# Then("I see number of items in the shopping cart increment up by 1") do
+  
+# end
+
+# Given(/^I have an item in my shopping cart$/) do
+#   click_button('ADD TO CART')
+#   binding.pry
+# end
+
+# When(/^I click on the remove button$/) do
+  
+# end
+
+# Then("I see the number of items in the cart decrease by 1") do
+  
+# end
+
+Given(/^I log into the products page with user "(.*?)"$/) do |user|
+  find('[data-test="username"]').send_keys(user)
+  find('[data-test="password"]').send_keys('secret_sauce')
+  find('.btn_action').click
+  have_selector('div.inventory_list')
+end
+
+When(/^I click on the shopping cart$/) do
+  click_on(class: 'shopping_cart_link')
+end
+
+Then("I can see the items I have selected") do
+  have_selector('div#cart_contents_container')
+end
+
+Given(/^I login with user "(.*?)" and enter the shopping cart$/) do |user|
+  find('[data-test="username"]').send_keys(user)
+  find('[data-test="password"]').send_keys('secret_sauce')
+  find('.btn_action').click
+  have_selector('div.inventory_list')
+  click_on(class: 'shopping_cart_link')
+end
+
+When(/^I click the Continue Shopping link$/) do
+  click_on(class: 'btn_secondary')
+end
+
+Then("I am returned to the products page") do
+  have_selector('div.inventory_list')
+end
+
+Given(/^I log into the products page with user "(.*?)"$/) do |user|
+  find('[data-test="username"]').send_keys(user)
+  find('[data-test="password"]').send_keys('secret_sauce')
+  find('.btn_action').click
+  have_selector('div.inventory_list')
+end
+
+When(/^I click on the hamburger menu and then on the About prompt$/) do
+  click_on(class: 'btn_secondary')
+end
+
+Then("I see the Sauce Labs home page") do
+  have_selector('div.inventory_list')
 end
